d845dc165814477e65e1ff30d79a996a76e2cfe9
Shahar Or <mightyiampresence@gmail.com>
2014-10-06 21:05:34 +0300

    Fix indentation karma conf.

--- karma.conf.js
+++ karma.conf.js
@@ -1,25 +1,25 @@
 /* eslint-env node */
 // Karma configuration
 module.exports = function(config) {
-  "use strict";
-  config.set({
-    basePath: "",
-    frameworks: ["mocha", "browserify"],
-    files: [
-        "test/*.js"
-    ],
-    preprocessors: {
-        "test/*.js": ["browserify"]
-    },
-    browserify: {
-        debug: true
-    },
-    reporters: ["progress"],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ["Chrome", "Firefox", "PhantomJS"],
-    singleRun: false
-  });
+    "use strict";
+    config.set({
+        basePath: "",
+        frameworks: ["mocha", "browserify"],
+        files: [
+            "test/*.js"
+        ],
+        preprocessors: {
+            "test/*.js": ["browserify"]
+        },
+        browserify: {
+            debug: true
+        },
+        reporters: ["progress"],
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: true,
+        browsers: ["Chrome", "Firefox", "PhantomJS"],
+        singleRun: false
+    });
 };
